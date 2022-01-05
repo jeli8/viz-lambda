4a412ededb07534a12319868d4c8f1646aab1248
Eliezer Yaacov <jeli8@users.noreply.github.com>
2022-01-05 23:35:22 +0200

    Adding a function to send an email

--- code/main.py
+++ code/main.py
@@ -1,5 +1,23 @@
 import mimetypes
 
+
+def send_email(emailContent)
+    from_mail = 'noreply-eli@gmail.com'
+    to_mail = 'eliezerj8@gmail.com'
+
+    s = smtplib.SMTP('host.docker.internal')
+    subject = 'Eli Yaacov - Viz home test'
+    print("Sending mail to %s", to_mail)
+
+    message = f"""\
+          Subject: {subject}
+          To: {to_mail}
+          From: {from_mail}
+          {emailContent}"""
+    result = s.sendmail(from_mail, to_mail, message)
+    s.quit()
+    
+
 def handler(event, context):
     sourceKey = event['Records'][0]['s3']['object']['key']
     (guessedType, encoding) = mimetypes.guess_type(sourceKey)
@@ -13,4 +31,5 @@ def handler(event, context):
         res += "UNKONWN"
     else:
         res += encoding
-    print(res)
+    print("File results is: %s", res)
+    send_email(res)
