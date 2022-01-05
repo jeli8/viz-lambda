bcab768857326c8bc4965f657188f8f3f1b56795
Eliezer Yaacov <jeli8@users.noreply.github.com>
2022-01-05 23:24:23 +0200

    Adding file type extraction from uploaded file to S3

--- code/main.py
+++ code/main.py
@@ -1,3 +1,16 @@
+import mimetypes
+
 def handler(event, context):
     sourceKey = event['Records'][0]['s3']['object']['key']
-    print(sourceKey)
+    (guessedType, encoding) = mimetypes.guess_type(sourceKey)
+    res = "Type is: "
+    if guessedType is None:
+        res += "UNKNOWN"
+    else:
+        res += guessedType
+    res += ", And file encoding is: "
+    if encoding is None:
+        res += "UNKONWN"
+    else:
+        res += encoding
+    print(res)
