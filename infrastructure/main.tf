fb09811ae01c017d2f90037c5eb6011ba6c9c741
Eliezer Yaacov <jeli8@users.noreply.github.com>
2022-01-06 02:22:21 +0200

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,12 +1,12 @@
- terraform {
+terraform {
    required_providers {
      aws = {
        source  = "hashicorp/aws"
-       version = "3.27.0"
+       Version = "~>3.27"
      }
    }
 
-   required_version = "0.13.3"
+   required_version = ">=0.13.3"
    backend "s3" {
      bucket = "viz-remote-states-bucket"
      key    = "viz-lambda/infra"
@@ -16,39 +16,39 @@
  }
 
  provider "aws" {
-   version = "3.27.0"
+   version = "~>3.0"
    region  = "us-east-1"
  }
 
  resource "aws_s3_bucket" "s3Bucket" {
    bucket = "viz-exam-input"
    acl    = "public-read"
-   policy = <<EOF
- {
-      "id" : "MakePublic",
-    "version" : "2012-10-17",
-    "statement" : [
-       {
-          "action" : [
-              "s3:*"
-           ],
-          "effect" : "Allow",
-          "resource" : [
-            "arn:aws:s3:::viz-exam-input/*",
-             "arn:aws:s3:::viz-exam-input"
-           ],
-          "principal" : "*"
-       }
-     ]
-   }
- EOF
+ }
 
+ resource "aws_lambda_permission" "allow_bucket" {
+   statement_id  = "AllowExecutionFromS3Bucket"
+   action        = "lambda:InvokeFunction"
+   function_name = aws_lambda_function.lambda.arn
+   principal     = "s3.amazonaws.com"
+   source_arn    = aws_s3_bucket.s3Bucket.arn
+ }
+
+ resource "aws_s3_bucket_notification" "bucket_notification" {
+   bucket = aws_s3_bucket.s3Bucket.id
+
+   lambda_function {
+     lambda_function_arn = aws_lambda_function.lambda.arn
+     events              = ["s3:ObjectCreated:Put"]
+     filter_prefix       = "AWSLogs/"
+     filter_suffix       = ".log"
+   }
+
+   depends_on = [aws_lambda_permission.allow_bucket]
  }
 
  resource "aws_lambda_function" "lambda" {
    role          = "arn:aws:iam::930094355358:role/service-role/send-file-info-role-0scifgyw"
    function_name = "send-file-info"
-   runtime       = "python3.8"
    package_type  = "Image"
    architectures = ["x86_64"]
 
